e1c82951325aecfe4b5803a6f81c8d3ba536a259
VenkateshLatchireddy <167878695+VenkateshLatchireddy@users.noreply.github.com>
2025-09-26 14:36:39 +0530

    .....

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/Navbar.css";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6"; // ensure fa6 is installed
+import { FaFacebookF, FaInstagram } from "react-icons/fa"; // removed unused FaTwitter
 
 const Navbar = () => {
   return (
@@ -11,18 +11,24 @@ const Navbar = () => {
 
       {/* Middle: Links */}
       <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Recipes</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">About Us</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#recipes">Recipes</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About Us</a></li>
       </ul>
 
       {/* Right: Social Icons */}
       <div className="social-icons">
-        <a href="#"><FaFacebookF /></a>
-        <a href="#"><FaXTwitter /></a>
-        <a href="#"><FaInstagram /></a>
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebookF />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaXTwitter />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
